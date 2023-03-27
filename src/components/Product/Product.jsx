8da2d8fb728ab66c3892a06ab161234ad94dc83c
Md Imon Hossain <imoncoc0@gmail.com>
2023-03-27 13:06:23 +0600

    Cart added

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
-import './Product.css'
+import './Product.css';
+
+
 
 const Product = (props) => {
     const {img, name, seller, ratings, quantity, price} = props.product;
+    const handleAddToCart = props.handleAddToCart;
+
     return (
       <div className="product">
         <img src={img} alt="" />
-        <div className='product-info'>
+        <div className="product-info">
           <h6 className="product-name">{name}</h6>
           <p>${price}</p>
           <p>Manufacturer: {seller}</p>
           <p>Rating: {ratings} Stars</p>
         </div>
-        <button className='btn-cart'>Add to Cart</button>
+        <button
+          onClick={() => handleAddToCart(props.product)}
+          className="btn-cart"
+        >
+          Add to Cart
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="cart-icon"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+                />
+              </svg>
+            </span>
+        </button>
       </div>
     );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,22 +4,32 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
     useEffect( () => {
         fetch('products.json')
         .then((res) => res.json())
         .then((data) => setProducts(data))
-    }, [])
+    }, []);
+
+    const handleAddToCart = (product) => {
+      const newCart = [...cart, product];
+      setCart(newCart)
+    };
+
     return (
         <div className='shop-container'>
             <div className="product-container">
                 {
                     products.map((product) => <Product
                     product={product}
-                    key={product.id}></Product>)
+                    key={product.id}
+                    handleAddToCart={handleAddToCart}></Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Order Summery</h4>
+                <p>Selected Item: {cart.length}</p>
             </div>
         </div>
     );
